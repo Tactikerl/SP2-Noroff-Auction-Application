b228d14dfb66da6c779fab5cb404e39acfc7c65d
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2022-11-18 21:48:49 +0100

    can display listings with html

--- js/listings.js
+++ js/listings.js
@@ -1,19 +1,50 @@
 // const listingsContainer = document.querySelector
 
 // fetch(`${API_AUCTION_URL}${AUCTION_LISTING}${AUCTION_BIDS_PARAMS}`)
-
-let raw = "";
+const listingsContainer = document.querySelector("#listingsContainer");
+const myHeaders = new Headers();
+let currentListings = [];
+myHeaders.append("Content-Type", "application/json");
 
 const requestOptions = {
   method: "GET",
-  body: raw,
-  redirect: "follow",
+  headers: myHeaders,
 };
 
 fetch(
   "https://nf-api.onrender.com/api/v1/auction/listings?limit=50&_seller=&_bids=",
   requestOptions
 )
-  .then((response) => response.text())
-  .then((result) => console.log(result))
+  .then((response) => response.json())
+  .then((listings) => {
+    currentListings = listings;
+    renderListings(listings);
+  })
   .catch((error) => console.log("error", error));
+
+function renderListings(listings) {
+  listings.forEach(function (getListings) {
+    listingsContainer.innerHTML += `<li class="d-flex" id="listObject">
+  
+  <div class="flex-grow-1 ms-3">
+     <h4 class="fs-5">
+         <a href="#fakelink">
+             ${getListings.title}
+         </a>
+     </h4>
+    
+     <h6 class="fs-6">
+         <small>
+             ${getListings.tags}
+         </small>
+     </h6>
+     <img class="media-object user-message"
+       src="${getListings.media}" alt="Image of ${getListings.title}"
+     <p>
+         ${getListings.description}
+     </p>
+     <hr>
+  </div>
+  </li>`;
+  });
+}
