ab1b35588a8b6ede31d5062fc4b672955c85c18a
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2022-12-01 09:56:28 +0100

    wip login functionality

--- js/login.js
+++ js/login.js
@@ -29,6 +29,66 @@
 // initSearchForm();
 import { API_AUCTION_URL, AUCTION_LOGIN, AUCTION_REGISTER } from "./api.js";
 
+const loginHTML = `
+  <form action="" class="row g-3" id="loginForm">
+      <div class="col-md-6">
+          <label for="userEmail"
+              class="form-label">Email</label>
+          <input type="email" class="form-control"
+              id="userEmail">
+      </div>
+      <div class="col-md-6">
+          <label for="userPassword"
+              class="form-label">Password</label>
+          <input type="password" class="form-control"
+              id="userPassword">
+      </div>
+  </form>`;
+
+const registerHTML = `
+  <form action="" class="row g-3" id="registerForm">
+      <div class="col-md-6">
+          <label for="newUserName"
+              class="form-label">Username</label>
+          <input type="text" class="form-control"
+              id="newUserName">
+      </div>
+      <div class="col-md-6">
+          <label for="newUserEmail"
+              class="form-label">Email</label>
+          <input type="email" class="form-control"
+              id="userEmail">
+      </div>
+      <div class="col-md-6">
+          <label for="userPassword"
+              class="form-label">Password</label>
+          <input type="password" class="form-control"
+              id="userPassword">
+      </div>
+      <div class="col-md-6">
+          <label for="repeatPassword"
+              class="form-label">Repeat Password</label>
+          <input type="text" class="form-control"
+              id="repeatPassword">
+      </div>
+      <div class="col-md-6">
+          <label for="avatar" class="form-label">Profile
+              Avatar</label>
+          <input type="url" name="avatar" id="profileAvatar">
+      </div>
+  </form>`;
+
+const btnLogin = document.querySelector("#btnradio1");
+//const btnRegister = document.querySelector("#btnradio2");
+const formCntr = document.querySelector("#formContainer");
+if (btnLogin.checked) {
+  formCntr.innerHTML = "";
+  formCntr.innerHTML += loginHTML;
+} else {
+  formCntr.innerHTML = "";
+  formCntr.innerHTML += registerHTML;
+}
+
 const myHeaders = new Headers();
 myHeaders.append("Authorization", `Bearer ${token}`);
 myHeaders.append("Content-Type", "application/json");
